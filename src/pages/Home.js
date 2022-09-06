4a29a2c1173fa820c2b66d7792bd1697fe385963
WadeTheDev <echo1006j@gmail.com>
2022-09-06 17:28:23 +0200

    avancement in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,14 @@ import H3 from "../components/H3";
 const Home = () => {
   return (
     <section className="home-container">
-    <div>
+    <article>
       <H1>COLORS.</H1>
       <H3>Drawing, Painting</H3>
-    </div>
+    </article>
+    <aside className="home-images">
+        <div className="bg-1"></div>
+        <div className="bg-2"></div>
+    </aside>
     </section>
   );
 };
