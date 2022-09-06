73dceeeb52b197a3294c1c8f75254324317a04d5
Malifarge <malifarge.thomas9@gmail.com>
2022-09-06 17:23:47 +0200

    first test count article

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,30 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Nav = () => {
+
+  const [cartQuantity,setCartQuantity] = useState(0)
+
+  const Count = () =>{
+    let test
+    const localArticlesIds= localStorage.getItem("articlesID")
+    if(localArticlesIds){
+      setCartQuantity(JSON.parse(test).length )
+    }
+     
+  }
+
+  useEffect(()=>{
+    Count()
+  },[])
+
   return (
     <header>
       <nav className="navbar">
         <Link className="home-link" to={"/"}>COLORS.</Link>
         <div>
         <Link to={"/Articles"}>Shop</Link>
-        <Link to={"/Panier"}>Cart(1)</Link>
+        <Link to={"/Panier"}>Cart({cartQuantity})</Link>
         </div>
       </nav>
     </header>
