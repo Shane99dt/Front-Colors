1c1c8c9554f484271bd6cb72b9179727e145fca7
WadeTheDev <echo1006j@gmail.com>
2022-09-06 17:05:33 +0200

    navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const Nav = () =>{
-    return(
-        <header>
-            <nav className="flex g-30 p-10 b-b">
-                <Link to={'/'}>Home</Link>
-                <Link to={'/Articles'}>Shop</Link>
-                <Link to={'/Panier'}>Cart(1)</Link>
+const Nav = () => {
+  return (
+    <header>
+      <nav className="navbar">
+        <Link className="home-link" to={"/"}>COLORS.</Link>
+        <div>
+        <Link to={"/Articles"}>Shop</Link>
+        <Link to={"/Panier"}>Cart(1)</Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-            </nav>
-        </header>
-    )
-}
-
-export default Nav
+export default Nav;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
-import H1 from "../components/H1"
-const Home = () =>{
-    return <>
-        <H1>Home</H1>
-    </>
-}
+import H1 from "../components/H1";
+import H3 from "../components/H3";
+const Home = () => {
+  return (
+    <section className="home-container">
+    <div>
+      <H1>COLORS.</H1>
+      <H3>Drawing, Painting</H3>
+    </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
