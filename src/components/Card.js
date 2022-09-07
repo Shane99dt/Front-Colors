bd0633063f04f4abaa566f8604de7d733d132108
WadeTheDev <echo1006j@gmail.com>
2022-09-07 11:52:27 +0200

    begining shop page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,13 @@
-import H3 from "./H3"
+import H3 from "./H3";
 
-const Card = ({image,title,price}) =>{
-
-    return ( 
-    <article>
-
-        <img src={image} alt={title} />
-        <H3>{title}</H3>
-        <p>{price}</p>
-
+const Card = ({ image, title, price }) => {
+  return (
+    <article className="card">
+      <img src={image} alt={title} />
+      <H3>{title}</H3>
+      <h4>{price}</h4>
     </article>
-    )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,9 +1,40 @@
-import H1 from "../components/H1"
+import H1 from "../components/H1";
+import Card from "../components/Card";
+import { useEffect, useState } from "react";
 
-const Articles = () =>{
-    return <>
-        <H1>Articles</H1>
+const Articles = () => {
+
+    const [products, setProduct] = useState([])
+    const [categories, setCatergories] = useState([])
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+    const fetchProducts = async () => {
+        const request = await fetch('https://e-commerce-fantastic4.herokuapp.com/products')
+        const response = await request.json()
+        setProduct(response)
+        console.log(response)
+    }
+
+  return (
+    <>
+    <div className="title-container">
+      <H1>SHOP</H1>
+    </div>
+    <section className="articles-container">
+        {/* {products.map((product) => {
+            return(
+                <Card 
+                image={''} 
+                title={''} 
+                price={''} />
+            )
+        })} */}
+    </section>
     </>
-}
+  );
+};
 
-export default Articles
+export default Articles;
