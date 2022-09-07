e53e61f9fb5c2f89d2e881c367b531ad8c0a5a7f
Malifarge <malifarge.thomas9@gmail.com>
2022-09-07 14:55:53 +0200

    404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,17 @@
 import H1 from "../components/H1"
+import H3 from "../components/H3"
 import Header from "../components/Header"
 
 const NotFound = () =>{
     return <>
         <Header length={localStorage.articlesID ? JSON.parse(localStorage.getItem("articlesID")).length : 0}/>
-        <H1>NotFound</H1>
+        <section className="flex center">
+            <article className="notFound-container">
+                <H1>404</H1>
+                <H3>Not Found</H3>
+            </article>
+        </section>
+
     </>
 }
 
