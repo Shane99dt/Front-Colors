af17b7837cf6d602be10be9a5843de6d1471214b
Malifarge <malifarge.thomas9@gmail.com>
2022-09-08 14:55:45 +0200

    button shop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
 import H1 from "../components/H1";
 import H3 from "../components/H3";
 import Header from "../components/Header";
+import Button from "../components/Button";
+
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
+  const handleEnterClick = () =>{
+    navigate("/Articles")
+  }
+
   return (
     <>
     <Header length={localStorage.articlesID ? JSON.parse(localStorage.getItem("articlesID")).length : 0}/>
@@ -11,6 +21,9 @@ const Home = () => {
       <article>
         <H1>COLORS.</H1>
         <H3>Drawing, Painting</H3>
+        <div className="ButtonEnter">
+          <Button text="Enter to the shop" handleClick={handleEnterClick}/>
+        </div>
       </article>
       <aside className="home-images">
           <div className="bg-1"></div>
