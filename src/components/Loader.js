e5a001f5196a691c328738cdd3ffd1e98ad9cc39
WadeTheDev <echo1006j@gmail.com>
2022-09-08 15:04:06 +0200

    logo

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import H1 from "./H1"
+
+const Loader  = () => {
+    return (
+        <div className="loading">
+         <H1>LOADER <span className="load-1">.</span><span className="load-2">.</span><span className="load-3">.</span></H1>   
+        </div>
+    )
+}
+
+export default Loader
